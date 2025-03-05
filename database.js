3d314b34175e14aab890904a8ba5f0119cb11bee
Alex Helton <birds1337@gmail.com>
2025-03-05 13:02:55 -0600

    we have db functionality now

--- database.js
+++ database.js
@@ -4,10 +4,21 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
     host: "localhost",
     user: "dinnerbird",
-    password: "yourpassword"
+    password: "buttsauce"
   });
   
   con.connect(function(err) {
     if (err) throw err;
-    console.log("Connected!");
+    console.log("It works?!");
+    var useStatement = "USE bogus_data"
+    con.query(useStatement, function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+    })
+    var selectTest = "SELECT * FROM bogus_data"
+    con.query(selectTest, function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+    })
   });
+
